7e07c15bb5bab20d6a22d3aa647011a480a626ce
Jesus <jesus.aranda7@yahoo.com>
2020-02-01 09:56:26 -0600

    final commit no more changes will be made refer to it for notes about querySelector

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+var newItemCounter = 1;
+
 var ourList = document.getElementById('our-list');
-
+//document.querySelector("#our-list);is the right
 var ourButton = document.getElementById('itemBtn');
-
-var ourHeadline = document.getElementById("our-headline");
+//for querySelector # for id and . for class
+var ourHeadline = document.getElementById('our-headline');
 
 var listItems = document.getElementById('our-list').getElementsByTagName('li')
+/*--- You can use querySelectorAll instead of get ElementById: document.querySelectorAll(#our-list li)
+making code less coversome ---*/
+ourList.addEventListener("click", activateItem);
 
-for (i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('click', activateItem)
+function activateItem(e) {
+  if(e.target.nodeName == "LI") {
+     ourHeadline.innerHTML = e.innerHTML;
+    for (i = 0; i < e.target.parentNode.children.length; i++) {
+        e.target.parentNode.children[i].classList.remove("active");
+      }
+      e.target.classList.add("active")
+     } 
 }
 
-function activateItem() {
-    ourHeadline.innerHTML = this.innerHTML;
-}
+
 
 ourButton.addEventListener('click', createNewItem);
 
 function createNewItem() {
-    ourList.innerHTML += "something new";
-    
+    ourList.innerHTML += "<li>something new " + newItemCounter + "</li>"; 
+    newItemCounter++;
 }
